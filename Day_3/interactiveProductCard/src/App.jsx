9cfc1f799ba6d918c0f0f5ad01c16628296f95ef
Taiwo Owoseje <taiwoowoseje@gmail.com>
2025-11-13 15:51:47 +0100

    Day 2 tasks in progress

---
+++ Day_3/interactiveProductCard/src/App.jsx
@@ -0,0 +1,11 @@
+import ProductCard from "./ProductCard";
+
+
+export default function App() {
+
+  return (
+    <div>
+      <ProductCard/>
+    </div>
+  )
+}
---
+++ Day_3/interactiveProductCard/src/ProductCard.jsx
@@ -0,0 +1,32 @@
+export default function ProductCard() {
+  return (
+    <div className="bg-blue-100 w-full h-screen flex justify-center items-center">
+      <div className="bg-white rounded-2xl shadow-lg  w-[300px] h-[400px] flex flex-col items-center transition-transform duration-500 hover:scale-105">
+        {/* Product Image */}
+        <div className="overflow-hidden rounded-xl">
+          <img
+            src="https://i.pinimg.com/736x/df/17/55/df17555a8ce3e504047cfb675208e1ee.jpg"
+            alt="Agbada"
+            className="w-[300px] h-64 object-cover rounded-xl transform transition-transform duration-500 hover:scale-110"
+          />
+        </div>
+
+        {/* Product Name */}
+        <p className="mt-4 text-xl font-semibold text-gray-800 transition-colors duration-300 hover:text-blue-700">
+          Agbada for Men
+        </p>
+
+        {/* Product Price */}
+        <p className="text-lg font-medium text-green-600">₦48,000</p>
+
+        {/* Add to Cart Button */}
+        <button
+          className="mt-4 bg-green-700 text-white w-32 h-10 rounded-lg font-medium
+                     transition-all duration-300 hover:bg-blue-800 active:scale-95"
+        >
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+}
